071de383bf6540b8ffbc8046cd2375bd086a130a
Sam Chung <hyochung@utexas.edu>
2024-01-29 10:10:25 -0600

    source with blank line, add_bytes.asm WIP

---
+++ assembler/add_bytes.asm
@@ -0,0 +1,41 @@
+    .ORIG x3000
+
+    LEA R4, ADR   ; R4 has the adress x3100
+    LDB R1, R4, x0  ; R1 has the first byte
+    LDB R2, R4, x1  ; R2 has the second byte
+
+    ADD R0, R1, R2  ; Perform addition
+
+    BRn NEGATIVE   ; Branch to NEGATIVE if the result is negative
+    BRp POSITIVE   ; Branch to POSITIVE if the result is positive
+    BRnzp ZERO     ; Branch to ZERO if the result is zero
+    
+
+NEGATIVE  
+    STB R0, R4, x2  ; Store sum in X3102
+    LD R3, #1      ; Prepare 1 to put in the register
+
+    STB R3, R4, x3 ; Store 1 in X3103 to indicate overflow
+    BR DONE
+
+    
+POSITVIE  
+    STB R0, R4, x2  ; Store sum in X3102
+    LD R3, #1      ; Prepare 1 to put in the register
+
+    STB R3, R4, x3 ; Store 1 in X3103 to indicate overflow
+    BR DONE
+
+
+    
+ZERO    
+    STB R0, R4, x2  ; Store sum in X3102
+    BR DONE
+
+
+    ADR .FILL x3100
+
+    DONE
+    HALT
+
+    .END
