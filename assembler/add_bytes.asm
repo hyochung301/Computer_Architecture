350aa102e9f914733c61fe9713e7f1e11c91cc90
Sam Chung <hyochung@utexas.edu>
2024-02-04 21:45:27 -0600

    store 0 for no overflow. Got 3/15

--- assembler/add_bytes.asm
+++ assembler/add_bytes.asm
@@ -1,42 +1,52 @@
+.ORIG x3000
 
-	; Name 1: Hyokwon Chung
-	; UTEID 1: hc27426
-    
-    .ORIG x3000
+LEA R4, ADR   ; R4 has the adress x3100
+LDB R1, R4, x0  ; R1 has the first byte
+LDB R2, R4, x1  ; R2 has the second byte
 
-    LEA R4, ADR   ; R4 has the adress x3100
-    LDB R1, R4, x0  ; R1 has the first byte
-    LDB R2, R4, x1  ; R2 has the second byte
+AND R5, R5, #0 ; Save the signs of R1 and R2 (R5 and R6 is neg indicator)
+ADD R5, R1, #0
+BRn R1NEGATIVE
+BRnzp R1XNEG ;300c
 
-    ADD R0, R1, R2  ; Perform addition
+R1NEGATIVE ADD R5, R5, #1 ;300e
 
-    BRn NEGATIVE   ; Branch to NEGATIVE if the result is negative
-    BRp POSITIVE   ; Branch to POSITIVE if the result is positive
-    BRnzp ZERO     ; Branch to ZERO if the result is zero
-    
+R1XNEG AND R6, R6, #0 ;;check R2 negative or not 3010
+ADD R6, R2, #0
+BRn R2NEGATIVE
+BRnzp R2XNEG ;3014
 
-NEGATIVE    STB R0, R4, x2  ; Store sum (R0) in X3102 (X3100 + 2)
-    AND R3, R3, #0      ; Prepare 1 at R3 to put in the register
-    ADD R3, R3, #1
-    STB R3, R4, x3 ; Store 1 in X3103 to indicate overflow
-    BR DONE
+R2NEGATIVE ADD R6, R6, #1
 
-    
-POSITVIE    STB R0, R4, x2  ; Store sum in X3102
-    AND R3, R3, #0      ; Prepare 1 at R3 to put in the register
-    ADD R3, R3, #1
-    STB R3, R4, x3 ; Store 1 in X3103 to indicate overflow
-    BR DONE
+R2XNEG ADD R0, R1, R2  ; Perform addition 3016
+STB R0, R4, x2  ; Store sum (R0) in X3102 (X3100 + 2)
+
+NOT R7, R5 ; Check for overflow if R1 and R2 have the same sign
+ADD R7, R7, #1
+ADD R7, R7, R6
+BRz SAMESIGN
+BRnzp DONE
+
+SAMESIGN AND R7, R7, #0
+ADD R7, R0, #0
+BRn R0NEGATIVE
+BRzp R0XNEG
+
+R0NEGATIVE ADD R7, R7, #1
+
+R0XNEG NOT R3, R5
+ADD R3, R3, #1
+ADD R3, R3, R7
+BRz DONE
+BRnzp OVERFLOW
+
+OVERFLOW AND R3, R3, #0      ; Prepare 1 at R3 to put in the register
+ADD R3, R3, #1
+STB R3, R4, x3 ; Store 1 in X3103 to indicate overflow
+BRnzp DONE
 
 
-    
-ZERO    STB R0, R4, x2  ; Store sum in X3102
-    BR DONE
-
-
-    ADR .FILL x3100
-
-    DONE
-    HALT
-
-    .END
+DONE ADD R0, R0, #0 ; Dummy code
+HALT
+ADR .FILL x3100
+.END
